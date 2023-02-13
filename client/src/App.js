b88e1cd7e9085e5174b11b5a515ade88eaf32302
Streafe <streafe@gmail.com>
2023-02-13 14:04:16 +0100

    project implementations

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
 import React from "react";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import HomePage from "pages/homePage";
+import LoginPage from "pages/loginPage";
+import ProfilePage from "pages/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
-  return <div className="app"></div>;
+  const { mode } = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import NavBar from "pages/navBar";
+import React from "react";
+
+function HomePage() {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+}
+
+export default HomePage;
